36b8daffa141b0675ceaffc10e39b93191ac4226
Sirio Balmelli <sirio@b-ad.ch>
2019-04-06 20:08:18 +0200

    must delete Gemfile.lock before all will be updated to latest version

--- script/generate_gemset_nix.sh
+++ script/generate_gemset_nix.sh
@@ -1,3 +1,4 @@
+rm -rfv Gemfile.lock
 nix-shell -p bundler \
 	--command 'bundler package --no-install --path vendor && rm -rf .bundler vendor' \
 	&& $(nix-build '<nixpkgs>' -A bundix)/bin/bundix \
