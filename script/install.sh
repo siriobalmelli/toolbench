06ad3329f7ae16546d9352c42111e53ea1fea4da
Sirio Balmelli <sirio@b-ad.ch>
2020-11-22 14:15:22 +0100

    install.sh: lint, remove coreutils dependency

--- script/install.sh
+++ script/install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #	install.sh
 # install (pin) a top-level Nix derivation as the Nix environment
 # (c) 2018 Sirio Balmelli
@@ -5,21 +6,21 @@
 # recipe mode
 set -e
 
-[[ $1 ]] && DERIVATION="$1" || DERIVATION="$(dirname $0)/../default.nix"
+[[ "$1" ]] && DERIVATION="$1" || DERIVATION="$(dirname "$0")/../default.nix"
 
 # install nix if necessary
 if ! command -v nix-env; then
 	curl https://nixos.org/nix/install | sh
-	source $HOME/.nix-profile/etc/profile.d/nix.sh
+	# shellcheck source=/Users/sirio/.nix-profile/etc/profile.d/nix.sh
+	source "$HOME/.nix-profile/etc/profile.d/nix.sh"
 	nix-env -iA "nixpkgs.git"  # some CentOs boxes have git 1.8, no -C flag
 fi
 
 # make sure there is a Git
 command -v git || nix-env -iA "nixpkgs.git"
-command -v nproc || nix-env -iA "nixpkgs.coreutils"
 
 # reset environment to current toolbench, clean up old generations
-nix-env --cores $(nproc) -f $DERIVATION -i --remove-all --tarball-ttl 0
+nix-env --cores 0 -j auto -f "$DERIVATION" -i --remove-all --tarball-ttl 0
 nix-env --delete-generations 10d
 
 # Set nix-env's environment to point to the nixpkgs we just built against.
@@ -38,5 +39,5 @@ nix-channel --update
 #nix-store --gc
 
 # execute post-install scripting
-[[ -s $(dirname $DERIVATION)/impurity.sh ]] && \
-	( cd $(dirname $DERIVATION) && ./impurity.sh )
+[[ -s "$(dirname "$DERIVATION")/impurity.sh" ]] && \
+	( cd "$(dirname "$DERIVATION")" && ./impurity.sh )
