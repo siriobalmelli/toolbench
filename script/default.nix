01bbfbd741067dc2ab76ac14c5d301024d2a936f
Sirio Balmelli <sirio@b-ad.ch>
2021-05-18 23:58:38 +0200

    zfsmon: fix stat print

--- script/default.nix
+++ script/default.nix
@@ -28,7 +28,7 @@ let
     disown %1
   '';
   tbh_zfsmon = writeShellScriptBin "tbh_zfsmon" ''
-    watch -- 'zpool iostat -yl; echo; zfs get usedbydataset,refcompressratio'
+    watch -- 'zpool iostat -yl 1 1; echo; zfs get usedbydataset,refcompressratio'
   '';
 
 in
